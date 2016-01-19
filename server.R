221d17b16403bfdb84eeacb8c7bbee1145fec063
dougiegee <dougcin@yahoo.com>
2016-01-18 22:57:57 -0500

    update titles and distribution
    Fixed distribution plot.  added titles

--- server.R
+++ server.R
@@ -1,9 +1,5 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+
 
 library(shiny)
 
@@ -12,27 +8,32 @@ shinyServer(function(input, output) {
   output$Capaplot <- renderPlot({
 
     
-    # capa graph
-    
-   # lsl<-80
-   # usl<-140
+    # distrbution plot
 
-    # determine plot scale
-    
-    probs<-seq(0.01, .99, .01)
+    # generate the x-variable for the distribution plot
+    probs<-seq(0.00135, .99865, .0001)
     quants<-qnorm(probs, input$mu, input$SD)
-     
+   
+    # determine the x-scale based on the spec limits or the normal curve 
+    # with .5*standard Deviation buffer
     ifelse(quants[1] < input$lsl, x.lower<-quants[1]-.5*input$SD, x.lower<-input$lsl - 0.5*input$SD) 
-    ifelse(quants[99] >input$usl, x.upper<-quants[99]+.5*input$SD, x.upper<-input$usl + 0.5*input$SD) 
+    ifelse(quants[length(probs)] >input$usl, x.upper<-quants[length(probs)]+.5*input$SD, x.upper<-input$usl + 0.5*input$SD) 
+    
+    
+    # y-values for the distribution curve
     yfit<-dnorm(quants, mean=input$mu, sd=input$SD) 
     
+   
+    # Calculation of Capbility Metric Cp and Cpk
     Cp<-round((input$usl-input$lsl)/(6*input$SD),2)
-    Cp.positionx <- (x.lower + .05*(x.upper-x.lower))
-    Cp.positiony<- yfit[40]
-    plot(quants, yfit, type="l", col="blue", lwd=5, xlim=c(x.lower,x.upper))
-    text(Cp.positionx, Cp.positiony, paste("Cp = ", Cp))
+    Cpk<-round(min((input$m-input$lsl)/(3*input$SD), (input$usl-input$mu)/(3*input$SD)),2)
+    
+    
+    #plot distribution with specs
+    plot(quants, yfit, type="l", col="blue", lwd=5, xlim=c(x.lower,x.upper), , xlab="Process Variable", ylab="")
     lines(c(input$lsl, input$lsl), c(0, .4), col="red",lwd=3)
     lines(c(input$usl, input$usl), c(0, .4), col="red",lwd=3)
+    title(main=(paste("Cp=", Cp, "  Cpk=", Cpk)))
     
 
   })
