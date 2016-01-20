e677cb98a333d69d202365e673e3aa9282b934cb
dougiegee <dougcin@yahoo.com>
2016-01-20 14:50:55 -0500

    finishing touches
    change some default

--- server.R
+++ server.R
@@ -8,19 +8,20 @@ shinyServer(function(input, output) {
   output$Capaplot <- renderPlot({
 
     
-    # distrbution plot
-
-    # generate the x-variable for the distribution plot (-3 to +3 standard dev)
-    probs<-seq(0.00135, .99865, .0001)
+    # distrbution plot 
+    # generate the x-variable for the Normal Density plot (-3 to +3 standard dev)
+    probs<-pnorm(seq(-3, 3, by=.05))
     quants<-qnorm(probs, input$mu, input$SD)
    
-    # determine the x-scale based on the spec limits or the normal curve 
-    # with .5*standard Deviation buffer
+    # determine the x-scale conditionally, based on the spec limits or the normal curve 
+    #  0.5*standard Deviation buffer on the min and max of x-scale
+    # lower value for x scale
     ifelse(quants[1] < input$lsl, x.lower<-quants[1]-.5*input$SD, x.lower<-input$lsl - 0.5*input$SD) 
+    # upper value for x scale
     ifelse(quants[length(probs)] >input$usl, x.upper<-quants[length(probs)]+.5*input$SD, x.upper<-input$usl + 0.5*input$SD) 
     
     
-    # y-values for the distribution curve
+    # y-values to plot the Normal density curve
     yfit<-dnorm(quants, mean=input$mu, sd=input$SD) 
     
    
@@ -29,8 +30,8 @@ shinyServer(function(input, output) {
     Cpk<-round(min((input$m-input$lsl)/(3*input$SD), (input$usl-input$mu)/(3*input$SD)),2)
     
     
-    #plot distribution with specs
-    plot(quants, yfit, type="l", col="blue", lwd=5, xlim=c(x.lower,x.upper), , xlab="Process Variable", ylab="")
+    #plot Normal Density Curve with Specification Limits 
+    plot(quants, yfit, type="l", col="blue", lwd=5, xlim=c(x.lower,x.upper), xlab="Process Variable", ylab="")
     lines(c(input$lsl, input$lsl), c(0, 40), col="red",lwd=3)
     text(input$lsl-.25*input$SD, dnorm(input$mu, mean=input$mu, sd=input$SD), "LSL")
     lines(c(input$usl, input$usl), c(0, 40), col="red",lwd=3)
--- ui.R
+++ ui.R
@@ -1,8 +1,15 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
+# Data Products Final Project
+# 
+# This project calculates capability indices given
+#    - SD :  Standard Deviation
+#    - mu :  Mean
+#    - lsl : Lower specification limit (lowest value for an acceptable item)
+#    - usl : Upper specification limit (highest value for an acceptable item)
 #
-# http://shiny.rstudio.com
+#  This app would be useful in a Six Sigma training environment to show users how these 
+#   commonly used metric behave based on the distribution of the data and the 
+#    allowable variation
 #
 
 library(shiny)
@@ -11,6 +18,8 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Capability Modeler"),
+  
+  # Some helper text using MathJax to describe the equations
   withMathJax(),
   helpText('Capability statistics Cp and Cpk are used in manufacturing to show the relationship between the variation of
                 a process (distribution) and the allowed variation'),
@@ -19,21 +28,27 @@ shinyUI(fluidPage(
   helpText('\\(Cpk=\\min\\left[\\left(\\frac{\\bar{x}-LSL}{6S}\\right),\\left(\\frac{USL-\\bar{x}}{6S}\\right)\\right]\\)'),
   helpText('Notice that the \\(Cpk\\) value is penalized when the mean is not exactly centered between the spec limits.'),
   
-  # Sidebar with a slider input for standard deviation, and numeric inputs for mean and specs
+  # Sidebar with a slider input for standard deviation, and mean, and numeric inputs for mean and specs
   sidebarLayout(
     sidebarPanel(
       sliderInput("SD",
                   "Standard Deviation:",
-                  min = .01,
+                  min = .0001,
                   max = 5,
                   value = 1),
-      numericInput(inputId="mu", value=0, label = "Enter Mean"),
-      numericInput(inputId="lsl", value=-6, label = "Enter Lower Specification (LSL)"),
-      numericInput(inputId="usl", value=6, label = "Enter Upper Specification (USL)")
+      sliderInput("mu",
+                  "Mean:",
+                  min = -30,
+                  max = 30,
+                  step = 0.5,
+                  value = 0),
+  
+      numericInput(inputId="lsl", value=-3, label = "Enter Lower Specification (LSL)"),
+      numericInput(inputId="usl", value=3, label = "Enter Upper Specification (USL)")
       
     ),
 
-    # Show a plot of the generated distribution
+    # Show a plot of the generated distribution and spec limits
     mainPanel(
 
        plotOutput("Capaplot")
